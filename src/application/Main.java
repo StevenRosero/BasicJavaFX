7197362dda1677eb7cec0487f9c84043e861e96d
Steven Rosero <steven.ros.cap@gmail.com>
2018-12-05 12:45:43 +0100

    Añadido el primer commit

---
+++ src/application/Main.java
@@ -0,0 +1,41 @@
+package application;
+	
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.effect.GaussianBlur;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	
+	private AnchorPane mypane;
+	public void start(Stage stage) throws IOException{// primero cargamos la vista fxml en el FXMLLoader
+		FXMLLoader loader = new FXMLLoader(getClass().getResource(
+				"view.fxml"));
+
+		// y opcionalmente un controlador para inicializar algún componente
+		loader.setController(new funciones());
+
+		// ahora sobre el panel mypane cargamos la vista que dejamos en el
+		// FXMLLoader
+		mypane = (AnchorPane) loader.load();
+		
+		// añade la escena al stage y la titula
+		stage.setTitle("Where's the squirrel?");
+		stage.setScene(new Scene(mypane));
+		stage.show();
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
